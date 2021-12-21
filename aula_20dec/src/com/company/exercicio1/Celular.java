186f9ba509cec0fe2e5a6c234780c44b1bf1cfe9
marcelonader <marcelo.nader@mercadolivre.com>
2021-12-21 12:18:46 -0300

    solução exercício 1 - aula 3

--- aula_20dec/src/com/company/exercicio1/Celular.java
+++ aula_20dec/src/com/company/exercicio1/Celular.java
@@ -1,5 +1,6 @@
 package com.company.exercicio1;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,26 +28,49 @@ public class Celular implements Precedente<Celular>{
         this.proprietario = proprietario;
     }
 
-    @Override
-    public int precedeA(Celular celular) {
-        Pattern pattern = Pattern.compile("[0-9]");
-        String numThisCelular = "";
-        String numParamCelular = "";
+//    @Override
+//    public int precedeA(Celular celular) {
+//        Pattern pattern = Pattern.compile("[0-9]");
+//        String numThisCelular = "";
+//        String numParamCelular = "";
+//
+//        for(int i = 0; i< this.getNumero().length(); i++){
+//            Matcher matcher = pattern.matcher(String.valueOf(this.getNumero().charAt(i)));
+//            if(matcher.find())
+//                numThisCelular += this.getNumero().charAt(i);
+//        }
+//
+//        for(int i = 0; i< celular.getNumero().length(); i++){
+//            Matcher matcher = pattern.matcher(String.valueOf(this.getNumero().charAt(i)));
+//            if(matcher.find())
+//                numParamCelular += celular.getNumero().charAt(i);
+//        }
+//
+//        return numThisCelular.compareTo(numParamCelular);
+//    }
+@Override
+public int precedeA(Celular celular) {
+    Pattern pattern = Pattern.compile("[0-9]");
 
-        for(int i = 0; i< this.getNumero().length(); i++){
-            Matcher matcher = pattern.matcher(String.valueOf(this.getNumero().charAt(i)));
-            if(matcher.find())
-                numThisCelular += this.getNumero().charAt(i);
-        }
+    String[] arrNumThisCelular = this.getNumero().split("");
+    String[] arrNumParamCelular = celular.getNumero().split("");
 
-        for(int i = 0; i< celular.getNumero().length(); i++){
-            Matcher matcher = pattern.matcher(String.valueOf(this.getNumero().charAt(i)));
-            if(matcher.find())
-                numParamCelular += celular.getNumero().charAt(i);
-        }
+    String nThis = Arrays.stream(arrNumThisCelular)
+            .reduce("", (acc,crr) ->  {
+                Matcher matcher = pattern.matcher(String.valueOf(crr));
+                    if(matcher.find())
+                        acc += crr;
+                    return acc;});
 
-        return numThisCelular.compareTo(numParamCelular);
-    }
+    String nParam = Arrays.stream(arrNumParamCelular)
+            .reduce("", (acc,crr) ->  {
+                        Matcher matcher = pattern.matcher(String.valueOf(crr));
+                        if(matcher.find())
+                            acc += crr;
+                        return acc;});
+
+    return nThis.compareTo(nParam);
+}
 
     @Override
     public String toString() {
